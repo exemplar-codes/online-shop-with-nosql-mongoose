167184fc0c90c511538aa19d8b9fd0e943a8f14b
Muhammad Sanjar Afaq <sanjarcode@gmail.com>
2023-03-19 15:15:50 +0530

    Add missing routes for shop

--- controllers/shop.js
+++ controllers/shop.js
@@ -1,15 +1,64 @@
 const Product = require("../models/Product");
 
-const getProducts = async (req, res, next) => {
-  const products = await Product.fetchAll();
-
-  res.render("shop/product-list", {
-    prods: products,
+const indexPage = async (req, res, next) => {
+  res.render("shop/index", {
     docTitle: "My shop",
     myActivePath: "shop-page",
   });
 };
 
+const getProducts = async (req, res, next) => {
+  const products = await Product.fetchAll();
+
+  res.render("shop/product-list", {
+    prods: products,
+    docTitle: "Products",
+    myActivePath: "/products",
+  });
+};
+
+const getProduct = async (req, res, next) => {
+  const products = await Product.fetchAll();
+
+  res.render("shop/product-list", {
+    prods: [products[0]],
+    docTitle: "Some product",
+    myActivePath: "/products-detail",
+  });
+};
+
+const cartPage = async (req, res, next) => {
+  res.render("shop/cart", {
+    docTitle: "Cart",
+    myActivePath: "/cart",
+  });
+};
+
+const checkoutPage = async (req, res, next) => {
+  res.render("shop/checkout", {
+    docTitle: "Checkout",
+    myActivePath: "/checkout",
+  });
+};
+
+const cartEditPage = async (req, res, next) => {
+  // res.render("shop/checkout", {
+  //   docTitle: "Checkout",
+  //   myActivePath: "/checkout",
+  // });
+  res.redirect("/");
+};
+
+const checkoutEditPage = async (req, res, next) => {
+  res.redirect("/");
+};
+
 module.exports = {
+  indexPage,
   getProducts,
+  cartPage,
+  checkoutPage,
+  getProduct,
+  cartEditPage,
+  checkoutEditPage,
 };
--- routes/shop.js
+++ routes/shop.js
@@ -6,6 +6,14 @@ const router = express.Router();
 
 const shopController = require('../controllers/shop');
 
-router.get('/', shopController.getProducts);
+router.get('/', shopController.indexPage);
+router.get('/products', shopController.getProducts);
+router.get('/products/:id', shopController.getProduct);
+router.get('/cart', shopController.cartPage);
+router.get('/checkout', shopController.checkoutPage);
+
+
+router.put('/cart', shopController.cartEditPage);
+router.put('/checkout', shopController.checkoutEditPage);
 
 module.exports = router;
