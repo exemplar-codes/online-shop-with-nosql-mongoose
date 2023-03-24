ea51668c60fcfdec84f710da9fc785a177b27c60
Muhammad Sanjar Afaq <sanjarcode@gmail.com>
2023-03-25 02:49:30 +0530

    Add Sequelize instance and export it

--- util/database.js
+++ util/database.js
@@ -1,13 +1,24 @@
-const mysql = require("mysql2");
 const env = require("dotenv").config().parsed;
-
 const database_password = env["MYSQL_DATABASE_PASSWORD"];
 
-const pool = mysql.createPool({
-  host: "localhost",
-  user: "root", // the user we set up in the MySQL Database
-  database: "node-complete", // name of the database
-  password: database_password, // password for the 'user' set up in the MySQL database
-}); // instead of .createConnection
+const Sequelize = require("sequelize");
 
-module.exports = pool.promise(); // return a promise since a pool does async ops (since it connects to database)
+// (database, username, password, options)
+const sequelize = new Sequelize("node-complete", "root", database_password, {
+  host: "localhost",
+  dialect: "mysql",
+});
+
+module.exports = sequelize;
+
+// No need to write the following. Sequelize does this for us
+// const mysql = require("mysql2");
+
+// const pool = mysql.createPool({
+//   host: "localhost",
+//   user: "root", // the user we set up in the MySQL Database
+//   database: "node-complete", // name of the database
+//   password: database_password, // password for the 'user' set up in the MySQL database
+// }); // instead of .createConnection
+
+// module.exports = pool.promise(); // return a promise since a pool does async ops (since it connects to database)
