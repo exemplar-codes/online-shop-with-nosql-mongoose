226967b78f8741422a48e901144ea69cc60470cf
Muhammad Sanjar Afaq <sanjarcode@gmail.com>
2023-05-19 01:16:45 +0530

    Remove Sequelize instance and export mongo instance instead

--- app.js
+++ app.js
@@ -1,22 +1,15 @@
 const path = require("path");
 
-const sequelize = require("./util/database");
-
 const express = require("express");
 const bodyParser = require("body-parser");
 const cors = require("cors");
 
 const app = express();
 
-const adminRoutes = require("./routes/admin");
-const shopRoutes = require("./routes/shop");
-const errorController = require("./controllers/error");
-const Product = require("./models/Product");
-const User = require("./models/User");
-const Cart = require("./models/Cart");
-const CartItem = require("./models/CartItem");
-const Order = require("./models/Order");
-const OrderItem = require("./models/OrderItem");
+// const adminRoutes = require("./routes/admin");
+// const shopRoutes = require("./routes/shop");
+// const errorController = require("./controllers/error");
+// const User = require("./models/User");
 
 // app.set('view engine', 'pug');
 // app.set('views', 'views'); // not needed for this case, actually
@@ -27,67 +20,17 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, "public")));
 app.use(cors());
 // mock authentication, i.e. get user who's making the request
-app.use(async (req, res, next) => {
-  req.user = await User.findByPk(1);
-  next();
-});
+// app.use(async (req, res, next) => {
+//   req.user = await User.findByPk(1);
+//   next();
+// });
 
 app.get("/try", async (req, res, next) => {
   await new Promise((r) => setTimeout(r, 1000));
   return res.json({ time: new Date().toLocaleTimeString() });
 });
 
-app.use("/admin", adminRoutes);
-app.use(shopRoutes);
+// app.use("/admin", adminRoutes);
+// app.use(shopRoutes);
 
-app.use(errorController.get404);
-
-// for the admin user, 1-N
-User.hasMany(Product);
-Product.belongsTo(User, { onDelete: "CASCADE" }); // syntax: talks about onDelete of target.
-// What it does here? Delete all products related to a user when the user is deleted
-
-// adding cart model, 1-1
-User.hasOne(Cart);
-Cart.belongsTo(User);
-
-// extra stuff, for ease of 'joined' pages
-// 1-
-// CartItem.hasOne(Product);
-// Product.belongsTo(CartItem); // no, doesn't make sense, OMIT
-/*
-  Also, Sequelize does not raise an error if model associations are inconsistent/nonsense.
-- It just creates all the tables, with links missing.
-- no errors, but 'afterBulkSync' does not run, which implies that Sequelize is aware of the unsuccessful "sync". So, why no errors reported? strange.
- */
-
-// N-M
-// Cart.hasMany(Product, { through: CartItem }); // correct, but Sequelize has weird notation, it forces `belongsToMany` on both sides.
-Cart.belongsToMany(Product, { through: CartItem });
-Product.belongsToMany(Cart, { through: CartItem });
-
-// For the magic methods, since they are absent.
-// Note: redundant from an SQL POV, since all FKs, indexes were added above.
-Cart.hasMany(CartItem);
-CartItem.belongsTo(Cart);
-
-// adding to avoid wasting time on query method quirks
-Product.hasMany(CartItem);
-CartItem.belongsTo(Product);
-
-// duplicating the Cart associations - for Order and OrderItem
-User.hasMany(Order);
-Order.belongsTo(User);
-Order.belongsToMany(Product, { through: OrderItem });
-Product.belongsToMany(Order, { through: OrderItem });
-Order.hasMany(OrderItem);
-OrderItem.belongsTo(Order);
-Product.hasMany(OrderItem);
-OrderItem.belongsTo(Product);
-
-sequelize
-  .sync()
-  .then(() => {
-    app.listen(process.env.PORT || 3000);
-  })
-  .catch(console.log);
+// app.use(errorController.get404);
--- util/database.js
+++ util/database.js
@@ -1,104 +1,23 @@
 const env = require("dotenv").config().parsed;
 /**
- * ENV keys: DBNAME, MYSQL_DATABASE_PASSWORD
+ * ENV keys: MONGODB_PASSWORD
  */
-const database_password = env["MYSQL_DATABASE_PASSWORD"];
-const database_name = env["DBNAME"];
+const database_password = env["MONGODB_PASSWORD"];
 
-const Sequelize = require("sequelize");
+const mongodb = require("mongodb");
+const MongoClient = mongodb.MongoClient;
 
-// work even if DB does not exist
-const sequelize = new Sequelize({
-  username: "root",
-  password: database_password,
-  host: "localhost",
-  dialect: "mysql",
+const mongoConnect = (callback) => {
+  MongoClient.connect(
+    `mongodb+srv://sanjarcode-nodejscompleteguide:${database_password}@cluster-nodejscompleteg.nuohpop.mongodb.net/?retryWrites=true&w=majority`
+  ) // Copied from the site (SRV address)
+    .then((client) => {
+      console.log("Connected!");
+      callback(client);
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+};
 
-  hooks: {
-    afterConnect: async (connection, options) => {
-      // connection is a mysql2 connection, since Sequelize used mysq2 under the hood/
-      // also, Sequelize assumes a DB is present. We are bypassing this by using the mysql2 part Sequelize exposes, since
-      // mysql2 has no "DB should exist" requirement. All it needs are creds and the DB to be running
-      await connection
-        .promise()
-        .query("CREATE DATABASE IF NOT EXISTS " + database_name);
-      options.database = database_name;
-    },
-    afterBulkSync: populateDatabaseWithSampleData,
-  },
-});
-
-// // (database, username, password, options)
-// const sequelize = new Sequelize("node-complete", "root", database_password, {
-//   host: "localhost",
-//   dialect: "mysql",
-//   hooks: {
-//     afterBulkSync: populateDatabaseWithSampleData,
-//   },
-// });
-
-module.exports = sequelize;
-
-async function populateDatabaseWithSampleData(x) {
-  const User = require("../models/User");
-  const Product = require("../models/Product");
-
-  const users = await User.findAll({ limit: 1 });
-
-  if (users.length > 0) return;
-
-  // 1. create a user
-  // const newUser = User.build(User.initialUsers[0]);
-  // await newUser.save();
-
-  // 2. create products associated with the user
-  const initialProducts = Product.initialProducts;
-  for (const sampleProd of initialProducts) {
-    /**
-     * Approach 1.0 - 'build' Product independently, save to DB, then associate the record
-     */
-    // const newProd = await Product.build(sampleProd);
-    // await newProd.save();
-    // await newUser.addProduct(newProd);
-    /**
-     * Approach 1.1 - 'create' a Product independently, then associate with the user record,
-     *   using User's mixin
-     * Minor variation of 2
-     */
-    // const newProd = await Product.create(sampleProd);
-    // await newUser.addProduct(newProd);
-    /**
-     * Approach 2 - 'create' a Product, using User instance's mixin, in one go.
-     */
-    // await newUser.createProduct(sampleProd);
-  }
-
-  /**
-   * Approach 3.1 - create the User, and associated Products all in one go
-   */
-  const user = await User.create(
-    {
-      ...User.initialUsers[0],
-      [Sequelize.Utils.pluralize(Product.name)]: initialProducts, // array of model instances would be OK too
-    },
-    { include: [{ model: Product }] }
-  );
-
-  // 3. Create a cart for the User
-  const cart = await user.createCart(); // cart doesn't have own data columns, so fine.
-  // const [product] = await Product.findAll({ limit: 1 });
-  // const cartItem = await cart.createCartItem({ quantity: 1 });
-  // await cartItem.setProduct(product);
-}
-
-// No need to write the following. Sequelize does this for us
-// const mysql = require("mysql2");
-
-// const pool = mysql.createPool({
-//   host: "localhost",
-//   user: "root", // the user we set up in the MySQL Database
-//   database: "node-complete", // name of the database
-//   password: database_password, // password for the 'user' set up in the MySQL database
-// }); // instead of .createConnection
-
-// module.exports = pool.promise(); // return a promise since a pool does async ops (since it connects to database)
+module.exports = mongoConnect; // is a function
