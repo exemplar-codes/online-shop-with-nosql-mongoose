4faf6edeaacd0bedb755709b386da475d82a1689
Muhammad Sanjar Afaq <sanjarcode@gmail.com>
2023-05-21 21:14:04 +0530

    Cosmetic change

--- routes/admin.js
+++ routes/admin.js
@@ -1,30 +1,30 @@
-const express = require('express');
+const express = require("express");
 
 const router = express.Router();
 
-const adminController = require('../controllers/admin')
+const adminController = require("../controllers/admin");
 
 // /admin/add-product => GET
-router.get('/add-product', adminController.getAddProduct);
+router.get("/add-product", adminController.getAddProduct);
 
 // /admin/add-product => GET
-router.get('/edit-product/:productId', adminController.getEditProduct);
+router.get("/edit-product/:productId", adminController.getEditProduct);
 
 // admin/products => GET list of products added by admin
-router.get('/products', adminController.getAdminProducts);
+router.get("/products", adminController.getAdminProducts);
 
 // /admin/add-product => POST
-router.post('/add-product', adminController.postAddProduct);
+router.post("/add-product", adminController.postAddProduct);
 
 // /admin/edit-product => POST
-router.post('/edit-product/:productId', adminController.postEditProduct);
+router.post("/edit-product/:productId", adminController.postEditProduct);
 // Note: alternatively we could have passed the id as a body, instead of using a dynamic param.
 // Both ways are OK, depending upon style/conventions of the codebase.
 
 // /admin/delete-product => POST
-router.post('/product/delete/:productId', adminController.deleteProduct);
+router.post("/product/delete/:productId", adminController.deleteProduct);
 
 // /admin/add-product => POST
-router.post('/delete-all-products', adminController.deleteAllProducts);
+router.post("/delete-all-products", adminController.deleteAllProducts);
 
 module.exports = router;
--- routes/shop.js
+++ routes/shop.js
@@ -1,22 +1,19 @@
-const path = require("path");
-
 const express = require("express");
 
 const router = express.Router();
 
-const shopController = require('../controllers/shop');
+const shopController = require("../controllers/shop");
 
-router.get('/', shopController.indexPage);
-router.get('/products', shopController.getProducts);
-router.get('/products/:productId', shopController.getProduct);
-router.get('/cart', shopController.cartPage);
-router.get('/checkout', shopController.checkoutPage);
-router.get('/orders', shopController.ordersPage);
-router.get('/orders/:orderId', shopController.orderPage);
+router.get("/", shopController.indexPage);
+router.get("/products", shopController.getProducts);
+router.get("/products/:productId", shopController.getProduct);
+router.get("/cart", shopController.cartPage);
+router.get("/checkout", shopController.checkoutPage);
+router.get("/orders", shopController.ordersPage);
+router.get("/orders/:orderId", shopController.orderPage);
 
-
-router.post('/cart', shopController.postCart);
-router.post('/orders', shopController.createOrder);
-router.put('/checkout', shopController.checkoutEditPage);
+router.post("/cart", shopController.postCart);
+router.post("/orders", shopController.createOrder);
+router.put("/checkout", shopController.checkoutEditPage);
 
 module.exports = router;
